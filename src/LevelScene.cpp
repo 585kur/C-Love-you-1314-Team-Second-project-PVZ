41a31b64e9405cd8060491f48b1c430869f7f83b
AaaaandyX <3287638761@qq.com>
2025-11-23 22:04:46 +0800

    Add files via upload
    Signed-off-by: AaaaandyX <3287638761@qq.com>

---
+++ src/LevelScene.cpp
@@ -0,0 +1,23 @@
+#include"LevelScene.h"
+#include<iostream>
+
+using namespace std;
+
+bool LevelScene::cell::AblePlant() {
+	if (PlantNum == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void LevelScene::ZombieCounter() {
+	//int zombieCount = 0;
+	//僵尸死亡函数内加上zombieCount++;
+};
+
+void SunshineCounter() {
+	
+};
+
