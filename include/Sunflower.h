365de8508941ab28857a29c4aabb3d9d1b17a5c2
585kur <1985714382@qq.com>
2025-11-27 21:18:55 +0800

    Update Sunflower.h

--- include/Sunflower.h
+++ include/Sunflower.h
@@ -3,20 +3,14 @@
 
 class Sunflower : public Plant {
 private:
-    int sun_production_rate; // 阳光生产速率 (帧)
-    int current_sun_cooldown;// 当前阳光冷却
+    int sun_production_rate;   // 阳光生产间隔（帧）
+    int current_sun_cooldown;  // 当前冷却
+    int width = 50;            // 向日葵自身宽度（固定尺寸）
+    int height = 50;           // 向日葵自身高度（固定尺寸）
 
 public:
     Sunflower(int x, int y);
-
-    // 重写绘制方法
-   // void draw() const override;
-    void draw();
-
-    // 重写更新逻辑
-    void update();
-
-    // 重写生产阳光方法
-    //int produce_sunshine() override;
-     int produce_sunshine();
+    void draw() const override;
+    void update() override;
+    int produce_sunshine() override;
 };
